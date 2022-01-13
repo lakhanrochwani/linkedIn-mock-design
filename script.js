0ed16fce89bb2dc700bc3d805409c60187e21d45
lakhanrochwani <rochwanilakhan@gmail.com>
2022-01-13 10:48:47 -0600

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('hello!')
+// let menu = document.getElementsByClassName('menu');
+// let submenu = document.getElementsByClassName('sub-menu');
+
+// menu.forEach(option => {
+//  option.addEventListener('click',(e) => {
+//   subMenuVisibility(e);
+//  })
+// })
+
+// function subMenuVisibility(e){
+//   let {child} = e.target;
+//   let arr = child;
+//   let lastchild = arr[arr.length-1];
+//   submenu.forEach(sub => {
+//   if(sub.classList.has('active')){
+//     sub.classList.remove('active')
+//   }
+//   })
+// })
+//   lastChild.classList.toggle('active');
+
+// }
